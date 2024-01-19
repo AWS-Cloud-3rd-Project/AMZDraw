10a685bbcd1b6c8be0201f5e043a3fb19f83fef0
woohyeoklee <whyeok94@gmail.com>
2024-01-19 14:23:36 +0900

    커밋 메시지

---
+++ src/test/java/com/amzmall/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amzmall.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
