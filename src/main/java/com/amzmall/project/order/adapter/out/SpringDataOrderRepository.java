b1effd952d09cb4059237cc9f73dfd765fb2e802
woohyeoklee <whyeok94@gmail.com>
2024-01-20 16:18:38 +0900

    feature findOrder

--- src/main/java/com/amzmall/project/order/adapter/out/SpringDataOrderRepository.java
+++ src/main/java/com/amzmall/project/order/adapter/out/SpringDataOrderRepository.java
@@ -1,6 +1,5 @@
 package com.amzmall.project.order.adapter.out;
 
-import com.amzmall.project.order.domain.OrderDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SpringDataOrderRepository extends JpaRepository<OrderJpaEntity, Long> {
