822a3b017a25b8bf7bc1ca89e2f72a6c0398d9ca
kimtj <kaminori1999@gmail.com>
2024-02-08 21:38:44 +0900

    feat : 구매 요청 기능 추가
    - 토스페이먼츠에 구매요청을 보내기위한 서비스 추가
    - 요청값에 대한 검증 후 결과에 따라 엔티티 객체로 만들어 반환
    - 추후에 토스페이먼츠에 전달 기능 추가 예정

--- src/main/java/com/amzmall/project/controller/PaymentController.java
+++ src/main/java/com/amzmall/project/controller/PaymentController.java
@@ -1,22 +1,31 @@
 package com.amzmall.project.controller;
 
 
-import com.amzmall.project.config.TossPaymentConfig;
+import com.amzmall.project.domain.dto.PaymentReqDto;
+import com.amzmall.project.domain.dto.PaymentResDto;
+import com.amzmall.project.exception.BusinessException;
+import com.amzmall.project.response.ResponseService;
+import com.amzmall.project.response.SingleResult;
 import com.amzmall.project.service.PaymentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/payments")
+@RequestMapping("/api/v1")
 public class PaymentController {
 
     private final PaymentService paymentService;
-    private final TossPaymentConfig tossPaymentConfig;
-
-
+    private final ResponseService responseService;
+    @PostMapping("/payment")
+    public SingleResult<PaymentResDto> requestPayments(
+            @ModelAttribute PaymentReqDto paymentReqDto
+    ) {
+       try {
+           return responseService.getSingleResult(paymentService.requestPayments(paymentReqDto));
+       } catch (Exception e){
+           e.printStackTrace();
+           throw new BusinessException(e.getMessage());
+       }
+    }
 }
--- src/main/java/com/amzmall/project/service/PaymentService.java
+++ src/main/java/com/amzmall/project/service/PaymentService.java
@@ -1,9 +1,13 @@
 package com.amzmall.project.service;
 
+import com.amzmall.project.advice.ExMessage;
+import com.amzmall.project.domain.dto.PaymentReqDto;
+import com.amzmall.project.domain.dto.PaymentResDto;
 import com.amzmall.project.domain.entity.Payment;
+import com.amzmall.project.config.TossPaymentConfig;
+import com.amzmall.project.exception.BusinessException;
 import com.amzmall.project.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,5 +17,35 @@ import org.springframework.transaction.annotation.Transactional;
 public class PaymentService {
     private final CustomerService customerService;
     private final PaymentRepository paymentRepository;
+    private final TossPaymentConfig tosspaymentConfig;
+    @Transactional
+    public PaymentResDto requestPayments(PaymentReqDto paymentReqDto){
+        Long amount = paymentReqDto.getAmount();
+        String payType = paymentReqDto.getPayType().name();
+        String customerEmail = paymentReqDto.getCustomerEmail();
+        String customerName = paymentReqDto.getCustomerName();
+        String orderName = paymentReqDto.getOrderName();
+        PaymentResDto paymentResDto;
 
+        if (amount == null || amount < 1000L){
+            throw new BusinessException(ExMessage.PAYMENT_ERROR_ORDER_PRICE);       // 주문 금액이 null이거나 1000원 미만이면 BusinessException 발생
+        }
+        if (!payType.equals("CARD") && !payType.equals("카드")){
+            throw new BusinessException(ExMessage.PAYMENT_ERROR_ORDER_PAY_TYPE);    // 결제 유형이 "CARD" 또는 "카드"가 아니면 BusinessException이 발생
+        }
+
+        try {
+            Payment payment = paymentReqDto.toEntity();
+            paymentResDto = payment.toPaymentDto();
+            paymentResDto.setSuccessUrl(tosspaymentConfig.getSuccessUrl());
+            paymentResDto.setFailUrl(tosspaymentConfig.getSuccessUrl());
+            paymentResDto.setCustomerEmail(customerEmail);
+            paymentResDto.setCustomerName(customerName);
+            paymentResDto.setPaymentKey("123456");
+            paymentRepository.save(payment);
+        }catch(Exception e){
+            throw new BusinessException(ExMessage.DB_ERROR_SAVE);
+        }
+        return paymentResDto;
+    }
 }
