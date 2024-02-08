919e19807f1d7fc0aaa52f57a48399f5022ab765
kimtj <kaminori1999@gmail.com>
2024-02-08 11:31:35 +0900

    refact : 결제 수단, 상태 ENUM 클래스 리팩터링
    - String 으로 반환하게끔 수정

--- src/main/java/com/amzmall/project/domain/entity/PayType.java
+++ src/main/java/com/amzmall/project/domain/entity/PayType.java
@@ -1,6 +1,13 @@
 package com.amzmall.project.domain.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum PayType {
-	CARD,
-	CASH;
+	CARD("카드"),
+	TOSSPAY("토스페이"),
+	VIRTUAL_ACCOUNT("가상계좌");
+	private final String name;
 }
--- src/main/java/com/amzmall/project/domain/entity/Status.java
+++ src/main/java/com/amzmall/project/domain/entity/Status.java
@@ -1,6 +1,12 @@
 package com.amzmall.project.domain.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Status {
-	A,
-	D;
+	A("사용 가능"),
+	D("사용 불가능");
+	private final String name;
 }
