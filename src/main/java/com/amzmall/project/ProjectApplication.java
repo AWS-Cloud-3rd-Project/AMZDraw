2307c3b41a7d1c096836debc670b2d4036c1ae65
kimtj <kaminori1999@gmail.com>
2024-02-08 11:46:20 +0900

    refact : JpaAuditing 기능 추가
    JpaAuditing 기능 사용을 위해 메인메서드에 @EnableJpaAuditing 어노테이션 추가

--- src/main/java/com/amzmall/project/ProjectApplication.java
+++ src/main/java/com/amzmall/project/ProjectApplication.java
@@ -1,8 +1,11 @@
 package com.amzmall.project;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ProjectApplication {
 
