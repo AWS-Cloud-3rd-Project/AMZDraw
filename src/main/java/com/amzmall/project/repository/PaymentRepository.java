cc9b62e733ba40e21d269190398d40f9742569ba
kimtj <kaminori1999@gmail.com>
2024-02-08 11:28:44 +0900

    refact : 결제 레포지터리 리팩터링
    - 클래스 이름 변경(JpaPayment -> Payment)

---
+++ src/main/java/com/amzmall/project/repository/PaymentRepository.java
@@ -0,0 +1,17 @@
+package com.amzmall.project.repository;
+
+import com.amzmall.project.domain.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+	Optional<Payment> findByOrderId(String orderId);
+
+	List<Payment> findAllByCustomerEmail(String customerEmail);
+
+}
