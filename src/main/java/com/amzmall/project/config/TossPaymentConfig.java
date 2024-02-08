a5b405fb918270fbee955a1e98daca368537a6a3
kimtj <kaminori1999@gmail.com>
2024-02-08 21:33:42 +0900

    feat : TossPayments 관련 설정 추가
    토스페이먼츠와 연동하기 위한 설정을 관리하는 TossPaymentConfig 클래스 추가

---
+++ src/main/java/com/amzmall/project/config/TossPaymentConfig.java
@@ -0,0 +1,24 @@
+package com.amzmall.project.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Getter
+public class TossPaymentConfig {
+    @Value("${toss.payments.client_key}")
+    private String testClientKey;
+
+    @Value("${toss.payments.secret_key}")
+    private String testSecretKey;
+
+    @Value("${toss.payments.success_url}")
+    private String successUrl;
+
+    @Value("${toss.payments.fail_url}")
+    private String failUrl;
+
+    // 토스페이먼츠 결제 승인 요청 URL
+    public static final String URL = "https://api.tosspayments.com/v1/payments/";
+}
