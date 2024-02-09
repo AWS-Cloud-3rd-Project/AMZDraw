c2baef8c293392cb3cbc9ef63f610d45601a29f7
kimtj <kaminori1999@gmail.com>
2024-02-09 21:34:32 +0900

    feat : 최종 결제 요청 기능 컨트롤러 추가
    Success URL 리다이렉트를 처리하는 컨트롤러 추가

--- src/main/java/com/amzmall/project/controller/PaymentController.java
+++ src/main/java/com/amzmall/project/controller/PaymentController.java
@@ -3,6 +3,7 @@ package com.amzmall.project.controller;
 
 import com.amzmall.project.domain.dto.PaymentReqDto;
 import com.amzmall.project.domain.dto.PaymentResDto;
+import com.amzmall.project.domain.dto.PaymentResSuccessDto;
 import com.amzmall.project.exception.BusinessException;
 import com.amzmall.project.response.ResponseService;
 import com.amzmall.project.response.SingleResult;
@@ -12,12 +13,13 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/payment")
 public class PaymentController {
 
     private final PaymentService paymentService;
     private final ResponseService responseService;
-    @PostMapping("/payment")
+
+    @PostMapping
     public SingleResult<PaymentResDto> requestPayments(
             @ModelAttribute PaymentReqDto paymentReqDto
     ) {
@@ -27,5 +29,27 @@ public class PaymentController {
            e.printStackTrace();
            throw new BusinessException(e.getMessage());
        }
+    }
+
+    @GetMapping("/success")
+    public SingleResult<PaymentResSuccessDto> requestFinalPayments(@RequestParam("paymentKey") String paymentKey,
+                                                     @RequestParam("orderId") String orderId,
+                                                     @RequestParam("amount") Long amount){
+        try{
+            System.out.println("paymentKey = " + paymentKey);
+            System.out.println("orderId = " + orderId);
+            System.out.println("amount = " + amount);
+            paymentService.verifyRequest(paymentKey, orderId, amount);
+            PaymentResSuccessDto result = paymentService.requestFinalPayment(paymentKey, orderId, amount);
+
+            return responseService.getSingleResult(result);
+        } catch (Exception e){
+            e.printStackTrace();
+            throw new BusinessException(e.getMessage());
+        }
+    }
+    @GetMapping("/fail")
+    public String r (){
+        return("fail.html");
     }
 }
