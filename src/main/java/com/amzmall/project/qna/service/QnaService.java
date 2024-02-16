9cd2c09ac3b16f698980b19018f38d60675b70ab
kimtj <kaminori1999@gmail.com>
2024-02-16 14:32:57 +0900

    fix : 답변 존재 시 예외처리 버그 수정
    답변이 존재해도 답변을 추가되는 버그 수정

--- src/main/java/com/amzmall/project/qna/service/QnaService.java
+++ src/main/java/com/amzmall/project/qna/service/QnaService.java
@@ -3,12 +3,17 @@ package com.amzmall.project.qna.service;
 import com.amzmall.project.advice.ExMessage;
 import com.amzmall.project.exception.BusinessException;
 import com.amzmall.project.qna.domain.dto.QuestionReqDto;
+import com.amzmall.project.qna.domain.dto.QuestionResDto;
 import com.amzmall.project.qna.domain.entity.Customer;
 import com.amzmall.project.qna.domain.entity.Question;
 import com.amzmall.project.qna.domain.entity.Reply;
 import com.amzmall.project.qna.repository.CustomerRepository;
 import com.amzmall.project.qna.repository.QuestionRepository;
+import com.amzmall.project.qna.repository.ReplyRepository;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,51 +22,45 @@ import org.springframework.transaction.annotation.Transactional;
 public class QnaService {
     private final QuestionRepository questionRepository;
     private final CustomerRepository customerRepository;
+    private final ReplyRepository replyRepository;
 
     @Transactional
     public void postQuestion(QuestionReqDto questionReqDto) {
-        boolean verify = verifyReq(questionReqDto);
-        if (!verify) throw new BusinessException(ExMessage.QUESTION_ERROR_REQUEST_FORM);
-
+        System.out.println(questionReqDto.getQuestionTitle());
+        System.out.println(questionReqDto.getQuestionContent());
+        System.out.println(questionReqDto.getCustomerEmail());
         Customer customer = customerRepository.findByEmail(questionReqDto.getCustomerEmail())
             .orElseThrow(() -> new BusinessException(ExMessage.CUSTOMER_ERROR_NOT_FOUND));
 
         try {
-            customer.addQna(questionReqDto.toEntity(customer.getEmail()));
+            customer.addQuestion(questionReqDto.toEntity(customer.getEmail()));
         } catch (Exception e) {
             throw new BusinessException(ExMessage.DB_ERROR_SAVE);
         }
 
     }
-
-    private boolean verifyReq(QuestionReqDto qnaReqDto) {
-        // 이메일이 존재하는지 확인
-        if (customerRepository.findByEmail(qnaReqDto.getCustomerEmail()).isEmpty())
-            return false;
-
-        return true;
-    }
-
     @Transactional
     public void postReply(Long questionId, String adminEmail, String replyContent) {
         // 질문 조회
         Question question = questionRepository.findById(questionId)
             .orElseThrow(() -> new BusinessException(ExMessage.QUESTION_ERROR_NOT_FOUND));
-        if (question.isReplied()) {
+        if (question.isReplied()) { // 답변 존재 시 예외처리
             throw new BusinessException(ExMessage.REPLY_ERROR_ALREADY_REPLIED);
-        }
-        Reply reply = new Reply();
-        reply.setAdmin(adminEmail);
-        reply.setReplyContent(replyContent);
-        question.setReply(reply);
-        // 답변 등록
-        try {
-            questionRepository.save(question);    // 변경된 질문 엔티티 저장
-        } catch (Exception e) {
-            throw new BusinessException(ExMessage.DB_ERROR_SAVE);
+        } else {
+            Reply reply = new Reply();
+            reply.setAdminEmail(adminEmail);
+            reply.setReplyContent(replyContent);
+            question.setReplied(true);
+            question.setReply(reply);
+            // 답변 등록
+            try {
+                replyRepository.save(reply);
+            } catch (Exception e) {
+                throw new BusinessException(ExMessage.DB_ERROR_SAVE);
+            }
         }
     }
-
+    // 문의 비활성화
     @Transactional
     public void unAvailableQuestion(Long questionId) {
         questionRepository.findByQuestionId(questionId)
