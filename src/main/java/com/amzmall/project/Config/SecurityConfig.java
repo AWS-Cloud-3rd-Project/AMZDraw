00b2c43d399c21b1134d05adb7a0ee148600f391
nyk <minseol1496@google.com>
2024-02-18 13:19:06 +0900

    feat:cognito 기본

--- src/main/java/com/amzmall/project/Config/SecurityConfig.java
+++ src/main/java/com/amzmall/project/Config/SecurityConfig.java
@@ -1,26 +1,34 @@
 package com.amzmall.project.Config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableGlobalMethodSecurity
 public class SecurityConfig {
     @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(); //해시 알고리즘 생성 후 반환(해시 기반 암호화)
+    }
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf()
-                .and()
-                .authorizeRequests(authz -> authz.mvcMatchers("/")
+        http.csrf(Customizer.withDefaults())
+                //.and()
+                .authorizeRequests(authz -> authz.requestMatchers("/")
                         .permitAll()
                         .anyRequest()
                         .authenticated())
-                .oauth2Login()
-                .and()
-                .logout()
-                .logoutSuccessUrl("/");
+                .oauth2Login(Customizer.withDefaults())
+                //.and()
+                .logout(httpSecurityLogoutConfigurer -> httpSecurityLogoutConfigurer.logoutSuccessUrl("/"));
         return http.build();
     }
 }
