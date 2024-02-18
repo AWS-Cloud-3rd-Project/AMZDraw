fd4557c75a293b5191186f2855bb3a1e24ff4a4d
nyk <minseol1496@google.com>
2024-02-18 12:33:32 +0900

    feat:cognito 기본

--- src/main/java/com/amzmall/project/Config/SecurityConfig.java
+++ src/main/java/com/amzmall/project/Config/SecurityConfig.java
@@ -1,5 +1,11 @@
 package com.amzmall.project.Config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
--- src/main/java/com/amzmall/project/Controller/SampleController.java
+++ src/main/java/com/amzmall/project/Controller/SampleController.java
@@ -1,5 +1,10 @@
 package com.amzmall.project.Controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 @Controller
 public class SampleController {
 
