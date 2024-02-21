681e543a2d608c8a95945aae982949f3649ad80b
nyk <minseol1496@google.com>
2024-02-21 23:53:33 +0900

    feat:security적용

--- src/main/java/com/amzmall/project/config/SimpleAuthenticationEntryPoint.java
+++ src/main/java/com/amzmall/project/config/SimpleAuthenticationEntryPoint.java
@@ -1,9 +1,12 @@
 package com.amzmall.project.config;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
 
 public class SimpleAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
--- src/main/java/com/amzmall/project/config/SimpleSecurityConfig.java
+++ src/main/java/com/amzmall/project/config/SimpleSecurityConfig.java
@@ -2,29 +2,51 @@ package com.amzmall.project.config;
 
 import com.amzmall.project.domain.customer.CustomerDetailService;
 import com.amzmall.project.enums.ECommerceRole;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Slf4j
 @Configuration
 @EnableWebSecurity
-public class SimpleSecurityConfig extends WebSecurityConfigurerAdapter {
-
+@RequiredArgsConstructor
+@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true)
+public class SimpleSecurityConfig {
     @Autowired
     private CustomerDetailService customerDetailService;
 
-    @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-                .csrf().disable()
-                .cors().disable();
+    @Bean
+    public static BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder(); //password암호화를 위해 BCryptPasswordEncorder클래스 생성하여 빈에 등록
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        return (web) -> web.ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }//resources 접근할 수 있도록 빈 추가
+    @Bean
+    //
+    //protected void configure(HttpSecurity httpSecurity) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                //html공격 막기 위한 csrf 비활성화
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/customer/")
+                        .cors().disable();
 
         httpSecurity
                 .formLogin()
