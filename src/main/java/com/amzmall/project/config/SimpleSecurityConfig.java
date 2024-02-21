c2f9cfad66336330826e2a733fa6930c706241db
nyk <minseol1496@google.com>
2024-02-22 01:04:41 +0900

    feat:security적용

--- src/main/java/com/amzmall/project/config/SimpleSecurityConfig.java
+++ src/main/java/com/amzmall/project/config/SimpleSecurityConfig.java
@@ -2,21 +2,29 @@ package com.amzmall.project.config;
 
 import com.amzmall.project.domain.customer.CustomerDetailService;
 import com.amzmall.project.enums.ECommerceRole;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
 
 @Slf4j
 @Configuration
@@ -38,24 +46,28 @@ public class SimpleSecurityConfig {
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }//resources 접근할 수 있도록 빈 추가
     @Bean
-    //
-    //protected void configure(HttpSecurity httpSecurity) throws Exception {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         //csrf 설정
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(requests -> requests
-                        .requestMatchers("/customer/","/customer/login","customer/signup").authenticated());
+                .authorizeHttpRequests((authz) -> authz
+                      //  .anyRequest().authenticated()
+                        .requestMatchers("/customer/","/customer/login","customer/signup").authenticated()
+                )
                 //customer의 메인페이지, 로그인 페이지, 회원가입 페이지는 로그인없이 접근 가능
                         //.cors().disable();
-
-        http
-                .formLogin()
+                .formLogin(formLogin -> formLogin
                 .loginPage("/customer/login")
                 .defaultSuccessUrl("/")
                 .usernameParameter("email")
-                .failureUrl("/customer/login?error=true")
-                .and()
+                        .successHandler(new AuthenticationSuccessHandler() {
+                            @Override
+                            public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                                System.out.println("authentication" + authentication.getName());
+                                response.sendRedirect("/");
+                            }
+                        })
+                .failureUrl("/customer/login?error=true"))
                 .logout()
                 .logoutSuccessUrl("/")
                 .invalidateHttpSession(true)
