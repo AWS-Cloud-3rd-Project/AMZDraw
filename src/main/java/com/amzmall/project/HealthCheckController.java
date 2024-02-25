996f2a33f01099878b2e40f44c317060906d342a
dhkim1206 <69470424+dhkim1206@users.noreply.github.com>
2024-02-25 20:54:40 +0900

    [DevOps] ADD : HealthCheck Conroller, application.properites

---
+++ src/main/java/com/amzmall/project/HealthCheckController.java
@@ -0,0 +1,15 @@
+package com.amzmall.project;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health-check")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("OK");
+    }
+}
