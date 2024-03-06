cd62ce853f91762a9c036607a0347fa8af4b3584
nyk <minseol1496@google.com>
2024-03-07 03:48:02 +0900

    feat:cognito 추가

--- src/main/java/com/amzmall/project/config/SwaggerConfig.java
+++ src/main/java/com/amzmall/project/config/SwaggerConfig.java
@@ -12,8 +12,8 @@ public class SwaggerConfig {
     public OpenAPI openAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Ecommerce market Swagger")
+                        .title("AMZMALL Swagger")
                         .version("0.1")
-                        .description("Ecommerce market Swagger docs"));
+                        .description("AMZMALL Swagger docs"));
     }
 }
