5de3dc65a8c23103a8aa0ed4884bf834c9659259
nyk <minseol1496@google.com>
2024-03-07 04:01:11 +0900

    feat:주석 추가

--- src/main/java/com/amzmall/project/controller/AuthController.java
+++ src/main/java/com/amzmall/project/controller/AuthController.java
@@ -13,6 +13,7 @@ public class AuthController {
 
     @PostMapping(value = "/sign-up")
     @ResponseBody
+    //코그니토 서비스 사용해서 회원가입 수행
     public String signUp(@RequestBody LogInRequestDto loginRequestDto){
         cognitoService.signUp(loginRequestDto.getUsername(), loginRequestDto.getPassword());
         return "sign-up";
@@ -20,17 +21,20 @@ public class AuthController {
 
     @PostMapping(value = "/login")
     @ResponseBody
+    ///코그니토 서비스 사용해서 로그인 수행하고 액세스 토큰 반환
     public String login(@RequestBody LogInRequestDto loginRequestDto){
         return cognitoService.login(loginRequestDto.getUsername(), loginRequestDto.getPassword());
     }
 
     @PostMapping(value = "/logout")
     @ResponseBody
+    //코그니토 서비스를 사용해서 전달된 액세스 토큰 사용해서 로그아웃 수행
     public String logout(@RequestBody String accessToken){
         cognitoService.logout(accessToken);
         return "logout";
     }
 
+    //세션 확인 요청 처리 - 전달된 액세스 토큰 사용하여 세션 확인 수행
     @PostMapping(value = "/check")
     @ResponseBody
     public String check(@RequestBody String accessToken){
