d75545916d6868592a1a82a955dbc051350e8d58
nyk <minseol1496@google.com>
2024-03-11 21:00:34 +0900

    feat:

--- src/main/java/com/amzmall/project/cognito/entity/User.java
+++ src/main/java/com/amzmall/project/cognito/entity/User.java
@@ -18,7 +18,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long user_id;
 
     private String username;
     private String password;
