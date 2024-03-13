bad25f6686c14c732b568d5436a1b853adef4781
ktj1999 <kimtj1999@naver.com>
2024-03-13 19:01:17 +0900

    refact : 인증 컨트롤러 엔드포인트 변경
    /auth -> /api/v1/auth

--- src/main/java/com/amzmall/project/cognito/controller/AuthController.java
+++ src/main/java/com/amzmall/project/cognito/controller/AuthController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/v1/auth")
 public class AuthController {
     @Autowired
     private CognitoService cognitoService;
