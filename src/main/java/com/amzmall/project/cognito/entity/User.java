8022616da642d8d9e96c7484244b12462359a0a3
nyk <minseol1496@google.com>
2024-03-13 22:55:29 +0900

    feat:

--- src/main/java/com/amzmall/project/cognito/entity/User.java
+++ src/main/java/com/amzmall/project/cognito/entity/User.java
@@ -1,19 +1,16 @@
 package com.amzmall.project.cognito.entity;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
 @Getter
 @Setter
 @NoArgsConstructor
 @Entity
+@Table(name = "user1")
 public class User {
 
     @Id
