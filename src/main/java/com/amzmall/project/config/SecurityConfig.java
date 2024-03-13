9ee68660768803ee5067602d7530aca95c597d31
ktj1999 <kimtj1999@naver.com>
2024-03-13 19:02:01 +0900

    refact : 스프링 시큐리티 설정 변경
    인증 컨트롤러 엔드포인트 변경으로 인한 requestMatchers 변경

--- src/main/java/com/amzmall/project/config/SecurityConfig.java
+++ src/main/java/com/amzmall/project/config/SecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration // 이 클래스를 스프링 설정 클래스로 지정
 @EnableWebSecurity // 웹 보안을 활성화
@@ -27,7 +26,7 @@ public class SecurityConfig{
                 )// 세션을 사용하지 않고, STATELESS 정책을 적용하여 매 요청마다 인증이 필요함
                 .authorizeHttpRequests((authorizeRequests)->
                         authorizeRequests // 요청에 대한 권한을 지정
-                .requestMatchers("/auth/login", "/auth/sign-up","/v3/**", "/swagger-ui/**").permitAll() // 로그인 및 회원가입 경로는 모두에게 접근 허용
+                .requestMatchers("/api/v1/auth/login", "/api/v1/auth/sign-up","/v3/**", "/swagger-ui/**").permitAll() // 로그인 및 회원가입 경로는 모두에게 접근 허용
                 .anyRequest().authenticated());// 그 외 모든 요청은 인증된 사용자만 접근 가능
                 //.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)); // JwtRequestFilter를 필터 체인에 추가하여 요청마다 토큰을 검증
         return http.build();
